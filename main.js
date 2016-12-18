6ff2d913db06fb588e83ca244d785baabf658808
Pierre De Wulf <pierredewulf31@gmail.com>
2016-12-18 23:11:57 +0100

    send mail

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var fetch = require('isomorphic-fetch');
+var nodemailer = require('nodemailer');
 
 var getMeteoReport = (location) => {
   const apiKey = 'fbe2c0e676679776b5de03ed5b309cce';
@@ -9,13 +10,28 @@ var getMeteoReport = (location) => {
       throw new Error('Bad response from server');
     }
     return response.json();
-  }).then((json) => json.weather[0].description);
+  }).then((json) => "Weather will be " + json.weather[0].description +
+                    " in " + location);
+};
+
+var sendMail = (report, mail) => {
+  var transporter = nodemailer.createTransport('smtps://rentacarcodinginterview%40gmail.com:dummydummy@smtp.gmail.com');
+
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+    from: '"Pierre de Wulf" <pierredewulf31@gmail.com>', // sender address
+    to: mail, // list of receivers
+    subject: 'Your weather report', // Subject line
+    text: report // plaintext body
+  };
+
+  transporter.sendMail(mailOptions);
 };
 
 module.exports = function (context, cb) {
   var city = context.data.city;
   var mail = context.data.mail;
-  getMeteoReport (city).then(function (weather) {
-    cb(null, weather);
+  getMeteoReport(city).then(function (weather) {
+    sendMail(weather, mail);
   });
 };
